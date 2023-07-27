57d30eaf7b13ca2232299781b4c68be25605bc78
danwen <danwen@penguin>
2023-07-27 16:39:18 -0400

    added init picture

--- multiverse-angular-start/src/app/date-picker/date-picker.component.ts
+++ multiverse-angular-start/src/app/date-picker/date-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NasaApiService } from '../nasa-api.service';
 
 @Component({
@@ -6,7 +6,7 @@ import { NasaApiService } from '../nasa-api.service';
   templateUrl: './date-picker.component.html',
   styleUrls: ['./date-picker.component.css']
 })
-export class DatePickerComponent {
+export class DatePickerComponent implements OnInit{
   selectedDate: Date;
   imageOfTheDay: any;
 
@@ -24,10 +24,26 @@ export class DatePickerComponent {
     )
   }
 
+  getInitPicture(){
+    const TodaysDate = "2023-07-27";
+    this.nasaApiService.getAstronomyPictureOfTheDay(TodaysDate).subscribe(
+      (data:any) => {
+        this.imageOfTheDay = data;
+      },
+      (error:any) => {
+        console.error(error);
+      }
+    )
+  }
+
   private formatDate(date: Date): string {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
     return `${year}-${month}-${day}`;
+  }
+
+  ngOnInit(): void {
+    this.getInitPicture();
   }
 }
